ae4819e6c0ee0c36d3183175947f854fe94e223b
huanghuanna418 <huanghuanna418@gmail.com>
2025-11-22 02:44:09 +0800

    Update storage.c

--- main/storage.c
+++ main/storage.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdbool.h>
+#ifndef _WIN32
+#include <strings.h> // for linux/mac user only because windows user is able to use _stricmp from string.h
+#define _stricmp strcasecmp
+#endif
+#include "cms.h"
 #include "storage.h"
 
 Student db[MAX_STUDENTS];
